581d69aa508bae61f09d2d7db7b24aa6977c6211
solarized-penguin <solarized.penguin@gmail.com>
2024-06-02 06:53:48 +0200

    creating initial db setup including data insert

--- web_api/db/models.py
+++ web_api/db/models.py
@@ -3,31 +3,39 @@ from enum import Enum
 from typing import Optional
 
 from sqlalchemy import Column
-from sqlalchemy.dialects.postgresql import TIMESTAMP
-from sqlmodel import SQLModel, Field, Relationship
+from sqlalchemy.dialects.postgresql import TIMESTAMP as PG_TIMESTAMP, ARRAY as PG_ARRAY, VARCHAR as PG_VARCHAR
+from sqlmodel import SQLModel, Field
+
+
+def _utc_now() -> datetime:
+    return datetime.now(UTC)
 
 
 class BookRating(SQLModel, table=True):
-    __tablename__ = "rating"
+    __tablename__ = "book_ratings"
 
-    book_id: int = Field(..., primary_key=True, foreign_key="book.id", title="Id of the rated book")
-    average: float = Field(0, title="Average rating")
-    votes: int = Field(0, title="Number of votes")
-    reviews: int = Field(0, title="Number of reviews")
+    book_id: int = Field(..., primary_key=True, foreign_key="books.id", title="Id of the rated book", nullable=False)
+    average: float = Field(0.0, title="Average rating", nullable=False)
+    votes: int = Field(0, title="Number of votes", nullable=False)
+    reviews: int = Field(0, title="Number of reviews", nullable=False)
 
 
 class Book(SQLModel, table=True):
-    __tablename__ = "book"
+    __tablename__ = "books"
 
     id: int | None = Field(None, primary_key=True, title="Primary key")
-    title: str = Field(..., title="Book title")
-    author: str = Field(..., title="Book author/authors")
-    isbn: str = Field(..., title="Isbn")
-    isbn13: str = Field(..., title="Isbn13")
-    language: str = Field(..., title="Language code")
-    pages: int = Field(..., title="Number of pages")
-    publication_date: date = Field(..., title="Publication date")
-    publisher: str = Field(..., title="Publisher")
+    title: str = Field(..., title="Book title", nullable=False)
+    authors: list[str] = Field(
+        ..., title="Book author/authors", sa_column=Column(PG_ARRAY(item_type=PG_VARCHAR, dimensions=1), nullable=False)
+    )
+    isbn: str = Field(..., title="Isbn", nullable=False)
+    isbn13: str = Field(..., title="Isbn13", nullable=False)
+    language: str = Field(..., title="Language code", nullable=False)
+    pages: int = Field(..., title="Number of pages", nullable=False)
+    publication_date: date = Field(
+        ..., title="Publication date", sa_column=Column(PG_TIMESTAMP(timezone=False), nullable=False)
+    )
+    publisher: str = Field(..., title="Publisher", nullable=False)
 
 
 class UserPrivileges(str, Enum):
@@ -41,14 +49,20 @@ class UserAccountStatus(str, Enum):
 
 
 class User(SQLModel, table=True):
-    __tablename__ = "user"
+    __tablename__ = "users"
 
-    id: Optional[int] = Field(None, primary_key=True, title="Primary Key of the user table")
-    email: str = Field(..., title="Email address", unique=True, index=True)
-    hashed_password: bytes = Field(..., title="Hashed Password", exclude=True)
-    username: str = Field(..., title="Username")
-    privileges: UserPrivileges = Field(UserPrivileges.Client, title="Privileges")
-    account_status: UserAccountStatus = Field(UserAccountStatus.Active, title="Account status")
+    id: Optional[int] = Field(None, primary_key=True, title="Primary Key")
+    email: str = Field(..., title="Email address", unique=True, index=True, nullable=False)
+    hashed_password: bytes = Field(..., title="Hashed Password", exclude=True, nullable=False)
+    username: str = Field(..., title="Username", nullable=False)
+    privileges: UserPrivileges = Field(UserPrivileges.Client, title="Privileges", nullable=False)
+    account_status: UserAccountStatus = Field(UserAccountStatus.Active, title="Account status", nullable=False)
+    created_at: datetime = Field(
+        default_factory=_utc_now,
+        title="Account creation date",
+        sa_column=Column(PG_TIMESTAMP(timezone=True), nullable=False),
+        exclude=True,
+    )
 
 
 class OrderStatus(str, Enum):
@@ -58,20 +72,17 @@ class OrderStatus(str, Enum):
     Cancelled = "Cancelled"
 
 
-def _utc_now() -> datetime:
-    return datetime.now(UTC)
-
-
 class Order(SQLModel, table=True):
-    __tablename__ = "order"
+    __tablename__ = "orders"
 
     id: Optional[int] = Field(None, primary_key=True, title="Primary Key of the order table")
     order_date: datetime = Field(
-        default_factory=_utc_now, title="Order date", sa_column=Column(TIMESTAMP(timezone=True), nullable=False)
+        default_factory=_utc_now, title="Order date", sa_column=Column(PG_TIMESTAMP(timezone=True), nullable=False)
     )
-    status: OrderStatus = Field(OrderStatus.Pending, title="Order status")
-    total_price: float = Field(..., title="Total order price")
-    user_id: int = Field(..., title="User ID", foreign_key="user.id")
+    status: OrderStatus = Field(OrderStatus.Pending, title="Order status", nullable=False)
+    total_price: float = Field(..., title="Total order price", nullable=False)
+    user_id: int = Field(..., title="User Id", foreign_key="users.id", nullable=False)
+
     # books: list[Book] = Relationship(back_populates="book", sa_relationship_kwargs={"lazy": "selectin"})
 
     @classmethod
